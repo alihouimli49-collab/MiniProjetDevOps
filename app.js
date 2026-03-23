81b43a43581eb8001ac4011eccf6610599ea6536
ilyes.teffaha <ilyes.teffaha@tunisietelecom.tn>
2026-03-23 15:54:46 +0100

    Update app.js message to v2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
  res.writeHead(200, {"Content-Type": "application/json"});
- res.end(JSON.stringify({ message: "Application DevOps opérationnelle" }));
+ res.end(JSON.stringify({ message: "Application DevOps opérationnelle by Ali v2" }));
 });
 server.listen(3000, () => {
  console.log("Serveur lancé sur le port 3000");
